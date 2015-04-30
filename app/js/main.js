18cd7db8ed42c3e894a0a4fdb0c4c05c53a637ab
ramtml <ramtml@outlook.com>
2015-04-30 20:25:50 +0530

    updated js files and index files

--- app/js/main.js
+++ app/js/main.js
@@ -16,15 +16,51 @@
     }
  
     Plugin.prototype.init = function () {
-        $(this.el).css('color', this.options.color);
+        //$(this.el).css('color', this.options.color);
         
-        var _divHeight = 0,
-        	_divWidth = 0,
+        var _defaultHeight = 0,
+        	_defaultWidth = 0,
         	_mousePoint = {
+                x : 0,
+                y : 0
+        	},
+            _ui = {
+               zoom : $('.zoom')
+            },
+            _zoomDiv,
+            _bgImg;
+            // Add the magnifying glass
+        if (_ui.zoom.length) {
+            var $div = $("<div class='glass'></div>");
+                _ui.glass = $div;
+                $('body').append($div);
+        }
 
-        	}
+        var mousemove = function (e){
+            var _zoomDiv = _bgImg.offset();
 
+            _mousePoint.x = e.pageX - _zoomDiv.left();
+            _mousePoint.y = e.pageY - _zoomDiv.top();
+
+            if( _mousePoint.x < _bgImg.width && _mousePoint.y < _bgImg.height && _mousePoint.x > 0 && _mousePoint.y > 0){
+                _zoomDiv(e);
+            }else{
+                _ui.glass.fadeOut(100);
+            }
+        }
+        
+        return;
+
+        var magnifier = function () {
+            
+        }
     };
+
+    Plugin.prototype.zoom = function () {
+        var mousemove = $(this),
+            mag
+
+    }
  
     $.fn.magnifier = function (options) {
         return this.each(function () {
